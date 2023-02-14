88b0843491521b85d7250bc4a26c2574d16655c5
Jliezed <jliezed@gmail.com>
2023-02-14 16:14:09 +0100

    update test

--- profiles/tests.py
+++ profiles/tests.py
@@ -11,16 +11,16 @@ class IndexViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('profiles:index'))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('profiles:index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
     def test_title(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('profiles:index'))
         self.assertContains(response, '<title>Holiday Homes</title>')
 
 
