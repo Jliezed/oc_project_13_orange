e726b22793084c16ca08d7bed7a704aefcb6bfcd
Jliezed <jliezed@gmail.com>
2023-02-26 10:57:33 +0100

    move data to new apps models

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -6,7 +6,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -14,12 +13,19 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Profile',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('favorite_city', models.CharField(blank=True, max_length=64)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.CreateModel(
+                    name='Profile',
+                    fields=[
+                        ('id',
+                         models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                        ('favorite_city', models.CharField(blank=True, max_length=64)),
+                        ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE,
+                                                      to=settings.AUTH_USER_MODEL)),
+                    ],
+                ),
             ],
-        ),
+            database_operations=[],
+        )
     ]
